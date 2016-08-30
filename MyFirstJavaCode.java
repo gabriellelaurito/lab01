ee3709c2bcb3cbe9c1cbe884b7b2ffa7f765f8bd
gabriellelaurito <gablaurito@gmail.com>
2016-08-30 12:13:37 -0400

    minor esit

--- MyFirstJavaCode.java
+++ MyFirstJavaCode.java
@@ -1,4 +1,4 @@
-public class <MyFirstJavaCode> {
+public class MyFirstJavaCode {
     public static void main(string[] args) {
       // Prints whatever you want in the terminal window
       System.out.println("Hello, World");
